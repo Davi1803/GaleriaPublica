623e25692c875cfc7f9bbd03cf3b611b0cd55dd6
20201tiimi0047 <sallescardosodavi@gmail.com>
2023-08-22 09:08:21 -0300

    pag 24

--- app/src/main/java/salles/cardoso/davi/galeriapublica/MainViewModel.java
+++ app/src/main/java/salles/cardoso/davi/galeriapublica/MainViewModel.java
@@ -6,19 +6,36 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelKt;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.paging.Pager;
+import androidx.paging.PagingConfig;
+import androidx.paging.PagingData;
+import androidx.paging.PagingLiveData;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import kotlinx.coroutines.CoroutineScope;
+
 public class MainViewModel extends AndroidViewModel {
 
     int navigationOpSelected = R.id.gridViewOp;
+     LiveData<PagingData<ImageData>> pageLv;
 
 
     public MainViewModel(@NonNull Application application){
         super(application);
-    }
+        GalleryRepository galleryRepository = new GalleryRepository(application);
+        GalleryPagingSource galleryPagingSource = new GalleryPagingSource(galleryRepository);
+        Pager<Integer, ImageData> pager = new Pager(new PagingConfig(10), () -> galleryPagingSource);
+        CoroutineScope viewModelScope = ViewModelKt.getViewModelScope(this);
+        pageLv = PagingLiveData.cachedIn(PagingLiveData.getLiveData(pager), viewModelScope);
+        }
+        public LiveData<PagingData<ImageData>> getPageLv() {
+        return pageLv;
+        }
     public int getNavigationOpSelected(){
         return navigationOpSelected;
     }
